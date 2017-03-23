ff572c9a3e49448631a795689ac60a951cb320fc
Fabricio <ravaneliferreira@hotmail.co.uk>
2017-03-23 15:48:47 +0000

    initial files and folders, shouldnt be functional yet:

---
+++ db/seeds.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+const { dbURI } = require('../config/environment');
+
+mongoose.connect(dbURI);
+
+mongoose.connection.close();
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const { port } = require('./config/environment');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoose.plugin(require('./lib/globalToJSON'));
+mongoose.Promise = require('bluebird');
+const { port, dbURI, env } = require('./config/environment');
 
 const app = express();
+mongoose.connect(dbURI);
 
+if(env !== 'test') app.use(morgan('dev'));
+
+app.use(express.static(`${__dirname}/public`));
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 app.listen(port, () => console.log(`Express is listening on port ${port}`));
