08ae54902a4d463d38d8652b3ec5ffce17ad546e
Fabricio <ravaneliferreira@hotmail.co.uk>
2017-03-23 16:27:52 +0000

    accidentally working in dev, woops.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 mongoose.plugin(require('./lib/globalToJSON'));
 mongoose.Promise = require('bluebird');
+const customResponses = require('./lib/customResponses');
+const errorHandler = require('./lib/errorHandler');
 const { port, dbURI, env } = require('./config/environment');
 
 const app = express();
@@ -10,7 +12,11 @@ mongoose.connect(dbURI);
 
 if(env !== 'test') app.use(morgan('dev'));
 
+app.use(customResponses);
+
 app.use(express.static(`${__dirname}/public`));
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
+
+app.use(errorHandler);
 app.listen(port, () => console.log(`Express is listening on port ${port}`));
